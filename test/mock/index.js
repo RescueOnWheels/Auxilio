6202e29e42583e3ffc9140ae680fb6591fde5e3c
Martijn Vegter <martijn@martijnvegter.com>
2018-11-19 10:31:54 +0100

    This is not a commit (#4)

--- test/mock/index.js
+++ test/mock/index.js
@@ -1,5 +1,7 @@
+const debug = require('./debug.test');
 const normalize = require('./normalize.test');
 
 describe('Mock', () => {
+  describe('debug', debug);
   describe('normalize', normalize);
 });
