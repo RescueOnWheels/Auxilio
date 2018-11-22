e5d9d788cb71223fd4d5027f873714601d655848
Martijn Vegter <mail@mvegter.me>
2018-11-22 19:48:11 +0100

    Added all helpers and tests from Motor.

--- test/mock/index.js
+++ test/mock/index.js
@@ -1,7 +1,11 @@
+const arraysEqual = require('./arraysEqual.test');
 const debug = require('./debug.test');
+const isDuplicate = require('./isDuplicate.test');
 const normalize = require('./normalize.test');
 
 describe('Mock', () => {
+  describe('arraysEqual', arraysEqual);
   describe('debug', debug);
+  describe('isDuplicate', isDuplicate);
   describe('normalize', normalize);
 });
