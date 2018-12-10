e83115b6856f969f7fd593102dd2ced05d04ba10
Nino van Galen <noxi333@users.noreply.github.com>
2018-12-10 14:21:24 +0100

    new repo

--- test/mock/index.js
+++ test/mock/index.js
@@ -2,10 +2,14 @@ const arraysEqual = require('./arraysEqual.test');
 const debug = require('./debug.test');
 const isDuplicate = require('./isDuplicate.test');
 const normalize = require('./normalize.test');
+const screen = require('./screen.test');
+const generateToken = require('./generateToken.test');
 
 describe('Mock', () => {
   describe('arraysEqual', arraysEqual);
   describe('debug', debug);
   describe('isDuplicate', isDuplicate);
   describe('normalize', normalize);
+  describe('screen', screen)
+  describe('generateToken', generateToken);
 });
