de14c88499d11223b13cae677891f46b508d260a
Martijn Vegter <martijn@martijnvegter.com>
2018-12-11 11:35:45 +0100

    It'd be nice if type errors caused the compiler to issue a type error (#12)

--- test/mock/index.js
+++ test/mock/index.js
@@ -1,15 +1,13 @@
 const arraysEqual = require('./arraysEqual.test');
 const debug = require('./debug.test');
+const generateToken = require('./generateToken.test');
 const isDuplicate = require('./isDuplicate.test');
 const normalize = require('./normalize.test');
-const screen = require('./screen.test');
-const generateToken = require('./generateToken.test');
 
 describe('Mock', () => {
   describe('arraysEqual', arraysEqual);
   describe('debug', debug);
+  describe('generateToken', generateToken);
   describe('isDuplicate', isDuplicate);
   describe('normalize', normalize);
-  describe('screen', screen)
-  describe('generateToken', generateToken);
 });
